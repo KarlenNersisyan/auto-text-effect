f7f2b487277d408eb3422c4849b9b5d496ba11bc
Karlen Nersisyan <karlennersisyan814@gmail.com>
2021-09-16 23:13:33 +0400

    Added new style

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const textEl = document.getElementById("text");
 const speedEl = document.getElementById("speed");
-const text = "We Love Programming!";
+const text = "I am Karlen Nersisyan😉😊";
 let idx = 1;
 let speed = 300 / speedEl.value;
 
